1771dada8f3c98d965d7ddf15970642caa33e9f5
AbdulRauf <mrrauf228@gmail.com>
2026-07-05 18:27:56 +0500

    flutter connected with backend(firebase)

--- habithub/lib/main.dart
+++ habithub/lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:habithub/firebase_options.dart';
 import 'package:habithub/views/splash_screen_view.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -14,7 +22,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'HabitHub',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF22C55E)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF22C55E),
+        ),
       ),
       home: const SplashScreenView(),
     );
