87d528e8d2a54daa85230ea6d8e5ec0aa1ab79ed
AbdulRauf <mrrauf228@gmail.com>
2026-08-02 17:21:38 +0500

    main file updated for a while to test the core funtion with skipping the auth system

--- habithub/lib/main.dart
+++ habithub/lib/main.dart
@@ -1,47 +1,91 @@
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:habithub/Auth/Bloc/auth_bloc.dart';
+// import 'package:habithub/Auth/services/auth_service.dart';
+// import 'package:habithub/Auth/user_Bloc/user_bloc.dart';
+// import 'package:habithub/firebase_options.dart';
+// import 'package:habithub/views/splash_screen_view.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+//   runApp(
+//   MultiBlocProvider(
+//     providers: [
+//       BlocProvider(
+//         create: (_) => AuthBloc(
+//           authService: AuthService(),
+//         ),
+//       ),
+
+//       BlocProvider(
+//         create: (_) => UserBloc(),
+//       ),
+//     ],
+//     child: const MyApp(),
+//   ),
+// );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'HabitHub',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF22C55E)),
+//       ),
+//       home: const SplashScreenView(),
+//     );
+//   }
+// }
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:habithub/Auth/Bloc/auth_bloc.dart';
-import 'package:habithub/Auth/services/auth_service.dart';
-import 'package:habithub/Auth/user_Bloc/user_bloc.dart';
+
 import 'package:habithub/firebase_options.dart';
-import 'package:habithub/views/splash_screen_view.dart';
+import 'package:habithub/views/main_navigation_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(
-  MultiBlocProvider(
-    providers: [
-      BlocProvider(
-        create: (_) => AuthBloc(
-          authService: AuthService(),
-        ),
-      ),
+  // TEMPORARY DEVELOPMENT LOGIN
+  //
+  // This bypasses the normal Splash/Auth flow so we can
+  // work on the core HabitHub application.
+  //
+  // Remove this when the real authentication flow is fixed.
 
-      BlocProvider(
-        create: (_) => UserBloc(),
-      ),
-    ],
-    child: const MyApp(),
-  ),
-);
+  final auth = FirebaseAuth.instance;
+
+  if (auth.currentUser == null) {
+    await auth.signInAnonymously();
+  }
+
+  runApp(const HabitHubDevApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HabitHubDevApp extends StatelessWidget {
+  const HabitHubDevApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'HabitHub',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF22C55E)),
-      ),
-      home: const SplashScreenView(),
+
+      theme: ThemeData(brightness: Brightness.dark, useMaterial3: true),
+
+      home: const MainNavigationView(),
     );
   }
 }
