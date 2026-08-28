a76ca6b943349c3bc17a4f7210af74ea29b33d9e
AbdulRauf <mrrauf228@gmail.com>
2026-08-28 23:26:17 +0500

    auth check code in main to test auth

--- habithub/lib/main.dart
+++ habithub/lib/main.dart
@@ -1,50 +1,50 @@
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:habithub/Auth/Bloc/auth_bloc.dart';
-// import 'package:habithub/Auth/services/auth_service.dart';
-// import 'package:habithub/Auth/user_Bloc/user_bloc.dart';
-// import 'package:habithub/firebase_options.dart';
-// import 'package:habithub/views/splash_screen_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habithub/Auth/Bloc/auth_bloc.dart';
+import 'package:habithub/Auth/services/auth_service.dart';
+import 'package:habithub/Auth/user_Bloc/user_bloc.dart';
+import 'package:habithub/firebase_options.dart';
+import 'package:habithub/views/splash_screen_view.dart';
 
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-//   runApp(
-//   MultiBlocProvider(
-//     providers: [
-//       BlocProvider(
-//         create: (_) => AuthBloc(
-//           authService: AuthService(),
-//         ),
-//       ),
+  runApp(
+  MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (_) => AuthBloc(
+          authService: AuthService(),
+        ),
+      ),
 
-//       BlocProvider(
-//         create: (_) => UserBloc(),
-//       ),
-//     ],
-//     child: const MyApp(),
-//   ),
-// );
-// }
+      BlocProvider(
+        create: (_) => UserBloc(),
+      ),
+    ],
+    child: const MyApp(),
+  ),
+);
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'HabitHub',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF22C55E)),
-//       ),
-//       home: const SplashScreenView(),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HabitHub',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF22C55E)),
+      ),
+      home: const SplashScreenView(),
+    );
+  }
+}
 
 // import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
